2419ecde3126253917cc456fdbbf41e35105ad54
Duy Tran <duytran840@learnit.fi>
2022-09-11 13:30:52 +0300

    Render login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Article from "pages/article/Article"
+import Authentication from "pages/authentication/Authentication"
 import GlobalFeed from "pages/globalFeed/GlobalFeed"
 import { Route, Switch } from "react-router-dom"
 
@@ -7,6 +8,7 @@ function App() {
 		<Switch>
 			<Route path="/" component={GlobalFeed} exact />
 			<Route path="/articles/:slug" component={Article} />
+            <Route path="/login" component={Authentication} />
 		</Switch>
 	)
 }
