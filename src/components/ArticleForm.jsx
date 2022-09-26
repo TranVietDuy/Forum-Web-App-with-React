88510ecb6c7bd4f488a25952b269deaecfb4a02a
Duy Tran <duytran840@learnit.fi>
2022-09-26 09:14:30 +0300

    add article form

--- src/components/ArticleForm.jsx
+++ src/components/ArticleForm.jsx
@@ -1,7 +1,108 @@
-const ArticleForm = () => {
-  return (
-    <div>ArticleForm</div>
-  )
+import BackendErrorMessages from "pages/authentication/components/BackendErrorMessages"
+import { useState, useEffect} from "react"
+
+const ArticleForm = ({ onSubmit, error, initialValues }) => {
+	const [title, setTitle] = useState("")
+	const [description, setDescription] = useState("")
+	const [body, setBody] = useState("")
+	const [tagList, setTagList] = useState("")
+
+    useEffect(() => {
+        setTitle(initialValues.title)
+        setDescription(initialValues.description)
+        setBody(initialValues.body)
+        setTagList(initialValues.tagList)
+       
+    }, [initialValues])
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const article = {
+            title,
+            description,
+            body,
+            tagList : tagList.split(" ")
+        }
+        onSubmit(article)
+    }
+
+	return (
+		<div className="editor-page">
+			<div className="container page">
+				<div className="row">
+					<div className="col-md-10 offset-md-1 col-xs-12">
+						{error && (
+							<BackendErrorMessages
+								backendErrors={error}
+							/>
+						)}
+						<form onSubmit={handleSubmit}>
+							<fieldset>
+								<fieldset className="form-group">
+									<input
+										type="text"
+										className="form-control form-control-lg"
+										placeholder="Article Title"
+										value={title}
+										onChange={(e) =>
+											setTitle(e.target.value)
+										}
+									/>
+								</fieldset>
+								<fieldset className="form-group">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="What's this article about?"
+										value={description}
+										onChange={(e) =>
+											setDescription(e.target.value)
+										}
+									/>
+								</fieldset>
+								<fieldset className="form-group">
+									<textarea
+										className="form-control"
+										rows="8"
+										placeholder="Write your article (in markdown)"
+										value={body}
+										onChange={(e) =>
+											setBody(e.target.value)
+										}
+									></textarea>
+								</fieldset>
+								<fieldset className="form-group">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Enter tags"
+										value={tagList}
+										onChange={(e) =>
+											setTagList(e.target.value)
+										}
+									/>
+									<div className="tag-list"></div>
+								</fieldset>
+								<button
+									className="btn btn-lg pull-xs-right btn-primary"
+									type="button"
+									onClick={() =>
+										onSubmit({
+											title,
+											description,
+											body,
+											tagList,
+										})
+									}
+								>
+									Publish Article
+								</button>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ArticleForm
--- src/pages/createArticle/index.js
+++ src/pages/createArticle/index.js
@@ -1,8 +1,27 @@
 import ArticleForm from "components/ArticleForm"
 
 const CreateArticle = () => {
+    const onSubmit = (data) => {
+        console.log("data", data)
+    }
+    const initialValues = {
+        title: "test",
+        description: "test",
+        body: "test",
+        tagList: "test",
+    }
+    const error = {
+        title: ["test"],
+        description: ["test"],
+        body: ["test"],
+        tagList: ["test"],
+    }
+
+
   return (
-    <div><ArticleForm /></div>
+		<div>
+			<ArticleForm onSubmit={onSubmit} initialValues={initialValues} />
+		</div>
   )
 }
 
